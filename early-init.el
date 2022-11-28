f2510d5d8ab687ca60cbd4083f769ea03c322e74
MountainMan <43313373+MountainMan1312@users.noreply.github.com>
2022-11-27 19:03:05 -0500

    Create configuration files
    Create early-init.el:
    - Add title and author
    - Enable lexical binding
    
    Create init.el:
    - Add title and author
    - Enable lexical binding

---
+++ early-init.el
@@ -0,0 +1,2 @@
+;;; early-init.el --- Early init file for MMacs -*- lexical-binding t; -*-
+;; Author: MountainMan
---
+++ init.el
@@ -0,0 +1,3 @@
+;;; init.el --- Init file for MMacs -*- lexical-binding: t; -*-
+;; Author:         MountainMan1312
+
