9175a356a68fb3bb206d48fb35ec0bc5a4c0775d
MountainMan <43313373+MountainMan1312@users.noreply.github.com>
2022-11-27 19:30:05 -0500

    Early Init & Package Management
    Add commentary section to early-init.el:
    - Description of early-init
    - Table of Contents
    
    Add main startup section to early-init.el:
    - Defer garbage collection until later
    - Start emacs-server
    - Hide mouse-based UI elements
    - Ignore Xresources
    - Fix for straight.el
    
    Add package management to init.el:
    - Don't load outdated code
    - Bootstrap straight.el
    - Add use-package
    - Add GCMH for garbage collection
    - Add async package

--- early-init.el
+++ early-init.el
@@ -1,2 +1,95 @@
 ;;; early-init.el --- Early init file for MMacs -*- lexical-binding t; -*-
 ;; Author: MountainMan
+
+;; ------------------------------------------------------------------------
+;;; COMMENTARY
+;; ------------------------------------------------------------------------
+
+;; ------------------------------------
+;; ABOUT EARLY INIT
+;; ----------------
+;; Early init allows configuration code
+;; to run early in the startup process,
+;; before the GUI loads.
+;; ------------------------------------
+
+;; ------------------------------------
+;; FILE CONTENTS
+;; -------------
+;; COMMENTARY
+;; - ABOUT EARLY INIT
+;; STARTUP
+;; - GARBAGE COLLECTION
+;; - EMACS SERVER
+;; - UI ADJUSTMENTS
+;; - IGNORE XRESOURCES
+;; - FIX FOR STRAIGHT.EL
+;; - END OF EARLY-INIT
+;; ------------------------------------
+
+
+
+
+;; ------------------------------------------------------------------------
+;;; STARTUP
+;; ------------------------------------------------------------------------
+
+;; ------------------------------------
+;; Garbage collection
+;; ------------------------------------
+;; Defer garbage collection.
+;; Garbage collection is handled by
+;; gcmh later in the startup process.
+
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+;; ------------------------------------
+
+;; ------------------------------------
+;; Emacs daemon/server
+;; -------------------
+(server-start)
+;; ------------------------------------
+
+;; ------------------------------------
+;; Remove ugly UI elements
+;; -----------------------
+;; Removing these UI elements before the
+;; GUI loads prevents a bit of stuttering
+
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+;; ------------------------------------
+
+;; ------------------------------------
+;; Inhibit implied resize
+;; ----------------------
+;; Resizing the Emacs frame can be a terribly
+;; expensive part of changing the font. By
+;; inhibiting this, we easily halve startup
+;; times with fonts that are larger than the
+;; system default.
+
+(setq frame-inhibit-implied-resize t)
+;; ------------------------------------
+
+;; ------------------------------------
+;; Ignore X resources
+;; ------------------
+;; It's settings would be redundant with the other
+;; settings in this file and can conflict with later
+;; config (particularly where the cursor color is concerned).
+
+(advice-add #'x-apply-session-resources :override #'ignore)
+;; ------------------------------------
+
+;; ------------------------------------
+;; Straight.el fix
+;; ---------------
+(setq package-enable-at-startup nil)
+;; ------------------------------------
+
+;; ------------------------------------------------------------------------
+;;; END OF EARLY-INIT
+;; ------------------------------------------------------------------------
--- init.el
+++ init.el
@@ -1,3 +1,114 @@
 ;;; init.el --- Init file for MMacs -*- lexical-binding: t; -*-
 ;; Author:         MountainMan1312
 
+;; ------------------------------------------------------------------------
+;;; COMMENTARY
+;; ------------------------------------------------------------------------
+
+;; ------------------------------------
+;; Configuration File Structure
+;; ----------------------------
+;; COMMENTARY
+;; STARTUP
+;; PACKAGE MANAGEMENT
+;; PERFORMANCE
+;; UI SETTINGS
+;; EXWM
+;; EDITOR
+;; SEARCH & COMPLETION
+;; PROJECT MANAGEMENT
+;; PROGRAMMING LANGUAGES
+;; SYSTEM ADMINISTRATION
+;; PKMS
+;; COMMUNICATIONS
+;; LAYOUT AUTOMATION
+;; KEYBINDS & CONTROLS
+;; ------------------------------------
+
+
+
+
+;; ------------------------------------------------------------------------
+;;; STARTUP
+;; ------------------------------------------------------------------------
+
+;; ------------------------------------
+;; EMACS SERVER
+;; ------------
+(server-start)
+;; ------------------------------------
+
+
+
+
+;; ------------------------------------------------------------------------
+;;; PACKAGE MANAGEMENT
+;; ------------------------------------------------------------------------
+
+;; ------------------------------------
+;; DON'T LOAD OUTDATED CODE
+;; ------------------------
+(setq load-prefer-newer t)
+;; ------------------------------------
+
+;; ------------------------------------
+;; STRAIGHT.EL
+;; -----------
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+;; ------------------------------------
+
+;; ------------------------------------
+;; USE-PACKAGE
+;; -----------
+(straight-use-package 'use-package)
+;; ------------------------------------
+
+;; ------------------------------------
+;; AUTOMATIC UPDATES
+;; -----------------
+(use-package auto-package-update
+  :straight t
+  :init
+  (auto-package-update-maybe)
+  (setq auto-package-update-hide-results t))
+;; ------------------------------------
+
+
+
+
+;; ------------------------------------------------------------------------
+;;; PERFORMANCE
+;; ------------------------------------------------------------------------
+
+;; ------------------------------------
+;; GARBAGE COLLECTION
+;; ------------------
+(use-package gcmh
+  :straight t)
+;; ------------------------------------
+
+;; ------------------------------------
+;; ASYNC
+;; -----
+;; Emacs is single-threaded by default.
+;; This package adds some asynchronous processing capability.
+(use-package async
+  :straight t)
+
+;; This enables asychronous copying, renaming, moving, etc. with Helm.
+(dired-async-mode 1)
+
+;; Compile emacs packages asynchronously on startup.
+(async-bytecomp-package-mode 1)
+;; ------------------------------------
